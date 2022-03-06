d645e9619d71d2ab9cf3b79816e39dafe872cacf
Alex81052 <alxdd87@gmail.com>
2022-03-06 14:57:54 -0500

    Challenge

---
+++ js/practice.js
@@ -0,0 +1,53 @@
+// import the data from data.js
+const tableData = data;
+
+// Reference the HTML table using d3
+var tbody = d3.select("tbody");
+
+// Simple JavaScript console.log statement
+function printHello() {
+    console.log("Hello there!");
+  }
+
+  // Takes two numbers and adds them
+function addition(a, b) {
+    return a + b;
+  }
+
+  console.log(addition(4, 5));
+
+  // Functions can call other functions
+function doubleAddition(c, d) {
+    var total = addition(c, d) * 2;
+    return total;
+  }
+
+  printHello = () => "Hello there!";
+
+  // Converted to an arrow function
+addition = (a, b) => a + b;
+
+// Original doubleAddition function
+function doubleAddition(c, d) {
+    var total = addition(c, d) * 2;
+    return total;
+  }
+
+  doubleAddition = (c, d)
+
+  => addition(c, d) * 2;
+
+  let friends = ["Sarah", "Greg", "Cindy", "Jeff"];
+
+  var vegetables = ["Carrots", "Peas", "Lettuce", "Tomatoes"];
+
+for (var i = 0; i < vegetables.length; i++) {
+    console.log("I love " + vegetables[i]);
+}
+
+for (var i = 0; i < 5; i++) {
+    console.log("I am " + i);
+ }
+
+ 
+
